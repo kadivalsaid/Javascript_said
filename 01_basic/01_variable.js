346bf05033aef5d80e12d3df7d04fa74a132176c
kadival said <kadivalsaid367@gmail.com>
2026-07-17 15:30:11 +0000

    data&variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId= 123321
+let accountEmail= "saidkadival@gmail.com"
+var accountPassword= "123456"
+accountCity= "Bangalore"
+
+//accountId= 456
+accountEmail= "dsasd@.cum"
+accountPassword= "123456789"
+accountCity= "Mumbai"
+console.log(accountEmail)
+console.log(accountId)
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
