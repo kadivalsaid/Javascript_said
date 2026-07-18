290908c3b5f095ce3498dbbbfcf68e10165b4985
kadival said <kadivalsaid367@gmail.com>
2026-07-18 02:54:27 +0000

    convertionofdatatypes

---
+++ 01_basic/convertionoperation.js
@@ -0,0 +1,19 @@
+let score = "44a"
+
+//string
+
+let valueInNumber= Number(score) //convert string to number
+//console.log(typeof valueInNumber) //number
+//console.log(valueInNumber) //NaN
+// "44" = 44
+// "44a" = NaN
+//let isLoginIn = "SAID"
+//let boll = Boolean(isLoginIn)
+//console.log(boll) //true
+
+let comeback = 77
+let stringValue = String(comeback)
+console.log(stringValue)
+console.log(typeof stringValue) //string
+
+
