1b87dd81008ecd8c47c0b1a99c0fdebfe8b38882
kadival said <kadivalsaid367@gmail.com>
2026-07-22 03:26:35 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,12 @@
+// premivite
+//7 types:string, number, boolean, null, undefined, symbol, BigInt
+
+
+
+//reference = non-premitive
+//object, array, function
+
+const id = Symbol("123");
+const id2 = Symbol("123");
+console.log(id===id2);
+ 
