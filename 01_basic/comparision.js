0e1a844c4e29490a48f23fa8855c398e23d4ace6
kadival said <kadivalsaid367@gmail.com>
2026-07-22 03:08:10 +0000

    comparision

---
+++ 01_basic/comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+
+//console.log(2==1)
+//console.log(1>"2");
+// let a = 10;
+// let b = 20;
+// console.log(a>b);
+// console.log(undefined == 0);
+//console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log("2" > 1);
+// console.log(2 > "1");
