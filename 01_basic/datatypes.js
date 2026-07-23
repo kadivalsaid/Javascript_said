e5a99b01409c2010ef1c39e23228307635ad1fdf
kadival said <kadivalsaid367@gmail.com>
2026-07-23 02:51:31 +0000

    sammary

--- 01_basic/datatypes.js
+++ 01_basic/datatypes.js
@@ -1,12 +1,27 @@
-// premivite
+// primitive
 //7 types:string, number, boolean, null, undefined, symbol, BigInt
 
-
-
-//reference = non-premitive
+//reference = non-primitive
 //object, array, function
 
 const id = Symbol("123");
 const id2 = Symbol("123");
 console.log(id===id2);
- 
+
+// const said= 12345678765432123456n;
+// console.log(typeof said);
+const hearos = ["spiderman", "ironman"];
+const hero = {
+    name: "said",
+    age: 20,
+};
+// console.log(hero);
+// console.log(hearos);
+// console.log(typeof hero);
+// console.log(typeof hero.age);
+// console.log(typeof hearos);
+const sayHello = function () {
+    console.log("hello");
+};
+console.log(typeof sayHello);
+//  console.log(sayHello());
