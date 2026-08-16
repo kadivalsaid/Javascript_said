c662b36fe044b3be74c436e7223f86a37f8abe61
kadival said <kadivalsaid367@gmail.com>
2026-08-16 15:10:30 +0000

    Add basic math operations and examples in nums-maths.js

---
+++ 01_basic/nums-maths.js
@@ -0,0 +1,36 @@
+// const score = 800
+// console.log(score);
+
+// const balance = new Number(100.4569466)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otesrnumber=213.85534
+// console.log(otesrnumber.toPrecision(4));
+
+// const hundrerds = 100000
+// console.log(hundrerds.toLocaleString('en-IN'));
+ 
+///*****************Maths****************/
+
+console.log(Math);
+console.log(Math.abs(-898));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+console.log(Math.max(50,8,5,5));
+
+console.log(Math.random());
+
+console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+ 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
